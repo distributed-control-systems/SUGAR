929747ba788a1b7bc1847e651d85a7c10275db70
papa <papa@192.168.1.76>
2023-11-11 20:36:50 +0100

    improving clean

--- @MV/clean.m
+++ @MV/clean.m
@@ -1,6 +1,17 @@
 function r= clean(self)
+[f,c]=size(self);
+if f*c==1
     k=self.vector;
     M=abs(k)>1e-6;
     r=MV(self.vector.*M,self.Lsignature,self.REPR);
+else
+    R=self;
+    for i=1:f
+        for j=1:c
+        R(i,j)=R(i,j).clean();
+        end
+    end
+    r=R
+end
 end
 
