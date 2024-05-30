5975d4b1148e27b0b014a345f8a7f939c76dfa8d
Manel Velasco <manel.velasco@upc.edu>
2024-05-30 17:11:34 +0200

    Differential Geometric algebra on course

--- Utility/KGA.m
+++ Utility/KGA.m
@@ -4,8 +4,8 @@ function KGA(sig,DTS,options)
 % p stands for number of positive basis, q for number of negative basis and
 % r for number of degenerate bases.
 % Additionally, it adds a tangent space to de algebra corresponding to the
-% description in DTS=[dimenssion, deep], where dimenssion is the number of
-% variables that will be used to compute the tangent space and deep is the
+% description in DTS=[dimenssion, order], where dimenssion is the number of
+% variables that will be used to compute the tangent space and order is the
 % maximum derivative order of these variables. The user has the
 % responsability to name this variables. 
 % Example:
@@ -26,10 +26,16 @@ function KGA(sig,DTS,options)
 % DD2 second derivative with respect to y
 
 %%
+% Probably this is the uglyest code I've done in my live, but I'm in a
+% hurry and this is what we get. At some point I will reformat it and erase
+% this comment.
+% The problem is that the cayley tables are defined with text strings and
+% matlab is horrible working with strings....😅
+
 GA(sig);
-n=sum(sig);
+%n=sum(sig);
 Names=e0.BasisNames;
-vec="["
+vec="[";
 for i=1:length(Names)
     vec=vec+" "+Names{i};
 end
@@ -38,7 +44,7 @@ M=eval(vec);
 tb=M.'*M;
 CAL=arrayfun(@(x) char(x),tb);
 CAL=strrep(CAL,"( 1 )*","");
-CAL=strrep(CAL,"( -1 )*","-")
+CAL=strrep(CAL,"( -1 )*","-");
 
 %Now... we have to suplement the table with the extra bases.... firs we
 %must create them
@@ -57,10 +63,10 @@ end
 % Don't do this at home, it's dangerous
 
 %%%%%%%% TRICK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-test=MV([1:2^sum(DTS)],[0 0 sum(DTS)]);
+test=MV(1:2^(sum(DTS)-1),[0 0 sum(DTS)-1]);
 New_mv=test.BasisNames;
 for i=sum(DTS)+1:length(New_mv)-1
-    index=char(strrep(New_mv{i+1},"e",""))
+    index=char(strrep(New_mv{i+1},"e",""));
     mv="";
     ind=[-1];
     for j=1:length(index)
@@ -75,37 +81,242 @@ for i=sum(DTS)+1:length(New_mv)-1
         end
     end
     if mv~=""
-        Extra=[Extra mv];
+        if (length(char(regexprep(mv,'[\d+]','')))<=DTS(2))
+            Extra=[Extra mv];
+        end
     end
     
 end
 %%%%%%%% END TRICK %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% and its related cayley table ....
+%Extra_cayley=[]
+for i=1:length(Extra) %rows
+    for j=1:length(Extra) %Columns
+        
+        mv=Extra(i)+Extra(j);
+        %lets count the derivative order
+        if (length(char(regexprep(mv,'[\d+]','')))>DTS(2))
+            result="0";
+        else
+            %lets count Ds before numbers
+            %Ds=0;
+            num=0;
+            mv=char(mv);
+            structure=zeros(DTS(1),1);
+            for k=1:length(mv)
+                if mv(k)=='D'
+                    num=num+1;
+                else
+                    structure(str2double(mv(k)))=structure(str2double(mv(k)))+num;
+                    num=0;
+                end
+            end
+            result="";
+            for k=1:DTS(1)
+                if structure(k)>0
+                    result=result+ char(ones(1,structure(k))*'D')+num2str(k);
+                end
+            end
+        end
+        Extra_cayley(i,j)=result;
+    end
+end
+
+%%%%%%%%%%%%%%% NEW-ORIGINAL=cors
 
 Extra_cross=[];
+elements_Extra_cross_cayley={};
 %Every extra element may end up multiplied by any of the already defined basis,
 % so...even more elements to the basis definition 
-for i=1:length(CAL(:,1))
+for i=1:length(CAL(2:end,1))
     for j=1:length(Extra)
-        Extra_cross=[Extra_cross CAL(i,1)+Extra(j)];
+        Extra_cross=[Extra_cross CAL(i+1,1)+Extra(j)];
+        elements_Extra_cross_cayley{end+1}=[CAL(i+1,1),Extra(j)];
+    end
+end
+%and some extra cayley sectors to compute
+
+
+for i=1:length(Extra_cross)
+    for j=1:length(Extra_cross)
+        %we must chek both caley tables that already exist
+        %the result is a mix
+        %find result in CAL
+        e1=elements_Extra_cross_cayley{i};
+        e2=elements_Extra_cross_cayley{j};
+        %where is e1(1) in the original cayey table
+        row=CAL(:,1)==e1(1);
+        %where is e2(1) in the original cayey table
+        column=CAL(1,:)==e2(1);
+
+        result=CAL(row,column);
+
+        %where is e1(2) in the Extra_cayley table
+        row=(Extra==e1(2));
+        %where is e2(2) in the Extra_cayley table
+        column=(Extra==e2(2));
+        
+        %is it 0?
+        if Extra_cayley(row,column)=="0"
+            result="0";
+        elseif CAL(row,column)=="0"
+            result="0"
+        else
+            % conmutativity of elements 
+            %how many basis in e1(2)?
+            left=length(char(regexprep(e1(2),"\d+","")));
+            %how many basis in e2(1)?
+            right= length(char(strrep(e2(1),"e","")));
+            
+            total=right*left;
+            s=(-1)^total;
+            if s==1
+                sign="";
+            else
+                sign="-";
+            end
+            result=strrep(sign+result+Extra_cayley(row,column),"--","");
+        end
+
+        Extra_cayley_cross(i,j)=result;
+    end
+end
+
+% Still missing some products....
+%%%%%%%%%%%%%%% ORIGINAL-CROSS
+%%%%%%%%%%%%%%% CROSS-ORIGINAL (signs!!!)
+for i=1:length(CAL(2:end,1))
+    for j=1:length(Extra_cross)
+            row=i+1;
+            e2=regexprep(Extra_cross(j),'D+\d+','');
+            e1=CAL(row,1);
+            column=CAL(1,:)==e2;
+            %Lets avoid the conmutativity by now and lets see what happens
+            start=regexp(Extra_cross(j),'D+\d+');
+            text=char(Extra_cross(j));
+
+            result=CAL(row,column)+text(start:end);
+        
+            Extra_cayley_cross_12(i,j)=result;
+            
+            % we have to cross e1 trough text(start:end) and change the
+            % sign due to the transpose...
+            %first, how mani bases will i cross in text(start:end)
+            
+            num_diff_bases=length(strrep(regexprep(text(start:end),'\d',''),"e","")); %number of Ds
+            % Extreme care must be taken, this will fail wheneber the bases
+            % go beyound 10...
+            num_crossing_bases=length(char(strrep(e1,"e","")));
+            total=num_crossing_bases*num_diff_bases;
+            
+            
+            s=(-1)^total;
+            if s==-1
+                sign="-";
+            else
+                sign="";
+            end
+            result=CAL(column,row)+text(start:end);
+            Extra_cayley_cross_21(j,i)=strrep(sign+result,"--","");
+    end
+end
+
+%%%%%%%%%%%%%%% ORIGINAL- NEW
+%%%%%%%%%%%%%%% NEW - ORIGINAL
+
+for i=1:length(CAL(2:end,1))
+    for j=1:length(Extra)
+            e1=CAL(i+1,1);
+            e2=Extra(j);
+            result=e1+e2;
+            Extra_cayley_cross_13(i,j)=result;
+
+            
+            num_diff_bases=length(regexprep(e2,'\d','')); %number of Ds
+            % Extreme care must be taken, this will fail wheneber the bases
+            % go beyound 10...
+            num_crossing_bases=length(char(strrep(e1,"e","")));
+            total=num_crossing_bases*num_diff_bases;
+            s=(-1)^total;
+            if s==1
+                sign="";
+            else
+                sign="-";
+            end
+            result=CAL(i+1,1)+e2;
+
+            Extra_cayley_cross_31(j,i)=strrep(sign+result,"--","");
+    end
+end
+
+%%
+%%%%%%%%%%%%%%% CROSS-NEW
+%%%%%%%%%%%%%%% NEW-CROSS
+for i=1:length(Extra_cross)
+    for j=1:length(Extra)
+            
+            e1=regexprep(Extra_cross(i),'D+\d+','');
+            %Lets avoid the conmutativity by now and lets see what happens
+            start=regexp(Extra_cross(i),'D+\d+');
+            text=char(Extra_cross(i));
+            e1_remain=text(start:end);
+            %which row do I find e1_remain?
+            row=Extra==e1_remain;
+             column=j;
+            e2=Extra_cayley(row,column);
+            if e2=="0"
+                Extra_cayley_cross_23(i,j)="0";
+            else
+                Extra_cayley_cross_23(i,j)=e1+e2;
+            end
+            
+            % Once again we have to count swaps of basis....
+            
+            left_new_basis=length(char((regexprep(Extra(j),"\d+",""))));
+            right_old_basis= length(char(strrep(regexprep(Extra_cross(i),"D+\d+",""),"e","")));
+            
+            %num of swaps
+            total=left_new_basis*right_old_basis;
+            s=(-1)^total;
+            if s==1
+                sign="";
+            else
+                sign="-";
+            end
+            if e2=="0"
+                Extra_cayley_cross_32(j,i)="0";
+            else
+                Extra_cayley_cross_32(j,i)=strrep(sign+e1+e2,"--","");
+            end
+
+    end
+end
+
+% Finally we have all the combinations!!!
+% Build the table
+FINAL=[CAL                                    [Extra_cross;Extra_cayley_cross_12 ] [Extra;Extra_cayley_cross_13]
+       [Extra_cross' Extra_cayley_cross_21 ]  Extra_cayley_cross                    Extra_cayley_cross_23 
+       [Extra' Extra_cayley_cross_31]        Extra_cayley_cross_32                Extra_cayley           ];
+        
+% Last round, remove those nasty e0
+for i=2:size(FINAL,1)
+    for j=2:size(FINAL,2)
+         fn=strrep(FINAL(i,j),"e0","");
+         if fn=="-" || fn==""
+
+         else
+             
+            FINAL(i,j)=fn;
+         end
     end
 end
 
 
-% Now we have all the new vectors and bivectors and trivectors and.... 
-% let's fill up the cayley table, which turns out to be a really big table!!!
-% the submatrix correspondoing to the derivatives is very simple
 
-
-
-
-Extra
-Extra_cross
-%%
-
-al="ℝ onto cayley table\n";
-
-
-if nargin==1
+sig_o=sig;
+sig=FINAL;
+al="Differential GA ["+ num2str(sig_o)+ "] ["+ num2str(DTS) +"]\n";
+if nargin==2
     options="";
 end
     m=length(sig(:,1));
@@ -150,16 +361,128 @@ if options=="verbose"
         fprintf("\n");
     end
 
-    algebra="The actual Algebra is ℝ onto cayley table\n" ;
+    algebra="The  Algebra is a Differential GA ["+ num2str(sig_o) + "] ["+ num2str(DTS) +"]\n" ;
     for i=1:m
         for j=1:m
             algebra=algebra+sig(i,j)+"\t";
-            al=al+sig(i,j)+"\t";
+            %al=al+sig(i,j)+"\t";
         end
             algebra=algebra+"\n";
-            al=al+"\n";
+            %al=al+"\n";
     end
    
 assignin('caller',"algebra_info",info(algebra))
 assignin('caller',"algebra",info(al))
+
+% % % % %%
+% % % % 
+% % % % al="ℝ onto cayley table\n";
+% % % % 
+% % % % 
+% % % % if nargin==1
+% % % %     options="";
+% % % % end
+% % % %     m=length(sig(:,1));
+% % % %     vec=zeros(1,m);
+% % % %     test=MV(vec,sig);
+% % % %     k=1;
+% % % %     vec=zeros(1,m);
+% % % %         vec(k)=1;
+% % % %         b=MV(vec,sig);
+% % % %         
+% % % %         vv=zeros(1,m);
+% % % %         vv(1)=1;
+% % % %         Basis=test.BasisNames;
+% % % %         if options=="verbose"
+% % % %         fprintf("Declaring [\b"+Basis{k}+"]\b as syntactic sugar, [\be0]\b=1\n")
+% % % %         
+% % % %         end
+% % % %         %algebra=algebra+"\t[\b"+Basis{k}+"]\b is syntactic sugar, [\be0]\b=1\n";
+% % % %         assignin('caller',Basis{k},b)
+% % % %   
+% % % %     for k=2:m
+% % % %         vec=zeros(1,m);
+% % % %         vec(k)=1;
+% % % %         b=MV(vec,sig);
+% % % %         
+% % % %         if options=="verbose"
+% % % %         fprintf("Declaring [\b" + Basis{k}+"]\b \n")
+% % % % 
+% % % %         end
+% % % %         %algebra=algebra+ "\t[\b"+ Basis{k}+"]\b \n";
+% % % %         assignin('caller',Basis{k},b)
+% % % %     end
+% % % % 
+% % % %     
+% % % % 
+% % % % if options=="verbose"
+% % % %     fprintf("which obey the cayley multiplication table\n")
+% % % %     for i=1:m
+% % % %         for j=1:m
+% % % %             fprintf(sig(i,j)+"\t");
+% % % %         end
+% % % %         fprintf("\n");
+% % % %     end
+% % % % 
+% % % %     algebra="The actual Algebra is ℝ onto cayley table\n" ;
+% % % %     for i=1:m
+% % % %         for j=1:m
+% % % %             algebra=algebra+sig(i,j)+"\t";
+% % % %             al=al+sig(i,j)+"\t";
+% % % %         end
+% % % %             algebra=algebra+"\n";
+% % % %             al=al+"\n";
+% % % %     end
+% % % %    
+% % % % assignin('caller',"algebra_info",info(algebra))
+% % % % assignin('caller',"algebra",info(al))
+% % % % end
+% % % % end
+% % % % 
+% % % %     
+% % % %     
+% % % %     function tbl=mix_tbl_generator(v1, v2 , C1, C2)
+% % % %     Extra_cross=[];
+% % % %     elements_Extra_cross_cayley={}
+% % % %     %Every extra element may end up multiplied by any of the already defined basis,
+% % % %     % so...even more elements to the basis definition 
+% % % % for i=1:length(v1)
+% % % %     for j=1:length(v2)
+% % % %         Extra_cross=[Extra_cross v1(i)+v2(j)];
+% % % %         elements_Extra_cross_cayley{end+1}=[v1(i),v2(j)];
+% % % %     end
+% % % % end
+% % % % %and some extra cayley sectors to compute
+% % % % 
+% % % % for i=1:length(Extra_cross)
+% % % %     for j=1:length(Extra_cross)
+% % % %         %we must chek both caley tables that already exist
+% % % %         %the result is a mix
+% % % %         %find result in CAL
+% % % %         e1=elements_Extra_cross_cayley{i};
+% % % %         e2=elements_Extra_cross_cayley{j};
+% % % %         %where is e1(1) in the original cayey table
+% % % %         row=find(CAL(:,1)==e1(1));
+% % % %         %where is e2(1) in the original cayey table
+% % % %         column=find(CAL(1,:)==e2(1));
+% % % % 
+% % % %         result=CAL(row,column);
+% % % % 
+% % % %         %where is e1(2) in the Extra_cayley table
+% % % %         row=find(Extra==e1(2));
+% % % %         %where is e2(2) in the Extra_cayley table
+% % % %         column=find(Extra==e2(2));
+% % % %         
+% % % %         %is it 0?
+% % % %         if Extra_cayley(row,column)=="0"
+% % % %             result="0";
+% % % %         else
+% % % %             %Lets avoid the conmutativity by now and lets see what happens
+% % % %             result=result+Extra_cayley(row,column);
+% % % %         end
+% % % % 
+% % % %         tbl(i,j)=result;
+% % % %     end
+% % % % end
+
 end
